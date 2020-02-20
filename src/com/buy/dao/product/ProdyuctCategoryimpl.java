75c6c9a2c5377f9d3aafd5ab1f51c51195cb257f
gg15137 <597196737@qq.com>
2020-02-20 10:58:17 +0800

    1.易买网搭建框架 2.使用easyCode自动生成实体类

--- src/com/buy/dao/product/ProdyuctCategoryimpl.java
+++ src/com/buy/dao/product/ProdyuctCategoryimpl.java
@@ -11,34 +11,51 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProdyuctCategoryimpl implements IProductCategory {
+    private Connection conn;
+    private PreparedStatement pstmt;
+    private ResultSet rs;
+
     @Override
     public List<EasybuyProductCategory> queryAllproductCategory(String parentId) {
         List<EasybuyProductCategory> productCategories = new ArrayList<EasybuyProductCategory>();
         EasybuyProductCategory productCategory = null;
+
         try {
             StringBuffer sql = new StringBuffer();
-            sql.append(" select * from easybuy_product_category  where 1=1  ");
-            //判断parentId的值 如果为0，显示的是一级分类
-            if (!"".equals(parentId)|| null!=parentId){
-               parentId = "0";
-               sql.append("and parentId ="+parentId);
+            sql.append("select * from easybuy_product_category where 1=1");
+
+            //判断parentID的值，如果为0，显示的是一级分类
+            if (!"".equals(parentId) && null != parentId) {
+                sql.append(" and parentId = ?");
             }
-            //获取链接
-            Connection conn = DataSourceUtil.getConn();
-            PreparedStatement pstmt = conn.prepareStatement(sql.toString());
-            ResultSet rs = pstmt.executeQuery();
-            while (rs.next()){
+
+            //获取连接
+            conn = DataSourceUtil.getConn();
+            pstmt = conn.prepareStatement(sql.toString());
+            if (!"".equals(parentId) && null != parentId) {
+                pstmt.setObject(1, parentId);
+            }
+
+            rs = pstmt.executeQuery();
+
+            //处理结果集
+            while (rs.next()) {
+                //实例化对象
                 productCategory = new EasybuyProductCategory();
                 productCategory.setId(rs.getInt(1));
                 productCategory.setName(rs.getString(2));
                 productCategory.setParentid(rs.getInt(3));
                 productCategory.setType(rs.getInt(4));
                 productCategory.setIconclass(rs.getString(5));
+                //将对象填充到集合中
                 productCategories.add(productCategory);
             }
-        }catch (Exception e){
-            System.out.println(e.getMessage());
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
         return productCategories;
     }
 }
+
